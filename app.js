f32216f5dccba8fd395d1aad17c81c3809b31613
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-07-28 17:26:48 -0300

    Sobre o tipo any

--- app.js
+++ app.js
@@ -1,51 +1,14 @@
 "use strict";
-const pessoa = {
-    nome: 'Mariana',
-    idade: 28,
-    profissao: 'dev'
-};
-pessoa.idade = 29;
-const andre = {
-    nome: 'Andre',
-    idade: 25,
-    profissao: 'pintor'
-};
-const paula = {
-    nome: 'Paula',
-    idade: 25,
-    profissao: 'Dev'
-};
-var Profissao;
-(function (Profissao) {
-    Profissao[Profissao["Professora"] = 0] = "Professora";
-    Profissao[Profissao["Atriz"] = 1] = "Atriz";
-    Profissao[Profissao["Desenvolvedora"] = 2] = "Desenvolvedora";
-    Profissao[Profissao["JogadorDeFutebol"] = 3] = "JogadorDeFutebol";
-})(Profissao || (Profissao = {}));
-const vanessa = {
-    nome: 'Vanessa',
-    idade: 23,
-    profissao: Profissao.Desenvolvedora
-};
-const maria = {
-    nome: 'Maria',
-    idade: 23,
-    profissao: Profissao.Atriz
-};
-const jessica = {
-    nome: 'Jessica',
-    idade: 28,
-    profissao: Profissao.Desenvolvedora,
-    materias: ['Matemática discreta', 'programação']
-};
-const monica = {
-    nome: 'Mônica',
-    idade: 28,
-    materias: ['Matemática discreta', 'programação']
-};
-function listar(lista) {
-    for (const item of lista) {
-        console.log('- ', item);
-    }
+let valorAny;
+valorAny = 3;
+valorAny = 'olá';
+valorAny = true;
+let valorString = 'teste';
+valorString = valorAny;
+let valorString2 = 'testão';
+valorString2 = valorAny;
+function somarString(string1, string2) {
+    console.log(string1 + string2);
 }
-listar(monica.materias);
+somarString(valorString, valorString2);
+somarString('ola, ', 'como vai?');
--- app.ts
+++ app.ts
@@ -1,69 +1,16 @@
-const pessoa = {
-    nome: 'Mariana',
-    idade: 28,
-    profissao: 'dev'
+let valorAny: any;
+valorAny = 3;
+valorAny = 'olá';
+valorAny = true;
+
+let valorString: string = 'teste';
+valorString = valorAny;
+let valorString2: string = 'testão'
+valorString2 = valorAny;
+
+function somarString(string1: string, string2: string){
+    console.log(string1 + string2);
 }
 
-pessoa.idade = 29;
-
-const andre: {nome: string, idade: number, profissao: string} = {
-    nome: 'Andre',
-    idade: 25,
-    profissao: 'pintor'
-}
-
-const paula: {nome: string, idade: number, profissao: string} = {
-    nome: 'Paula',
-    idade: 25,
-    profissao: 'Dev'
-}
-
-enum Profissao {
-    Professora,
-    Atriz,
-    Desenvolvedora,
-    JogadorDeFutebol
-}
-
-interface Pessoa {
-    nome: string,
-    idade: number,
-    profissao?: Profissao
-}
-
-interface Estudante extends Pessoa {
-    materias: string[]
-}
-
-const vanessa: Pessoa = {
-    nome: 'Vanessa',
-    idade: 23,
-    profissao: Profissao.Desenvolvedora
-}
-
-const maria: Pessoa = {
-    nome: 'Maria',
-    idade: 23,
-    profissao: Profissao.Atriz
-}
-
-const jessica: Estudante = {
-    nome: 'Jessica',
-    idade: 28,
-    profissao: Profissao.Desenvolvedora,
-    materias: ['Matemática discreta', 'programação']
-}
-
-const monica: Estudante = {
-    nome: 'Mônica',
-    idade: 28,
-    materias: ['Matemática discreta', 'programação']
-}
-
-function listar(lista: string[]){
-    for( const item of lista ) {
-        console.log('- ', item);
-    }
-}
-
-listar(monica.materias);
+somarString(valorString, valorString2);
+somarString('ola, ', 'como vai?');
