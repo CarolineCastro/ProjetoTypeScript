cd59d6bb3c30caac6873b3a8cf67c22dbdadeb97
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-07-28 19:29:54 -0300

    tsconfig: strictNullChecks e noImplicitAny

--- dist/app.js
+++ dist/app.js
@@ -3,3 +3,6 @@ let buttonTest = document.getElementById('button');
 buttonTest === null || buttonTest === void 0 ? void 0 : buttonTest.addEventListener('click', () => {
     console.log('Funcionou');
 });
+function somarImplicito(item1, item2) {
+    console.log(item1 + item2);
+}
--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,7 @@ let buttonTest = document.getElementById('button');
 buttonTest?.addEventListener('click', () => {
     console.log('Funcionou');
 });
+
+function somarImplicito(item1, item2){
+    console.log(item1 + item2);
+}
