8e044297326d4444f0e5b0704591f3016525b7c4
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-07-28 19:01:43 -0300

    tsconfig: target e lib

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
 "use strict";
-let anyVoltou;
-anyVoltou = 3;
-anyVoltou = 'teste';
-anyVoltou = 5;
-let stringTest = 'verificar';
-stringTest = anyVoltou;
-let unknownValue;
-unknownValue = 3;
-unknownValue = 'opa';
-unknownValue = true;
-unknownValue = 'vai sim';
-let stringTest2 = 'agora vai';
-//stringTest2 = unknownValue;
-if (typeof unknownValue === 'string') {
-    stringTest2 = unknownValue;
-}
-function jogaErro(erro, codigo) {
-    throw { error: erro, code: codigo };
-}
-jogaErro('deu erro', 500);
+let buttonTest = document.getElementById('button');
+buttonTest === null || buttonTest === void 0 ? void 0 : buttonTest.addEventListener('click', () => {
+    console.log('Funcionou');
+});
--- app.ts
+++ app.ts
@@ -1,26 +1,5 @@
-let anyVoltou: any;
-anyVoltou = 3;
-anyVoltou = 'teste';
-anyVoltou = 5;
+let buttonTest = document.getElementById('button');
 
-let stringTest: string = 'verificar';
-stringTest = anyVoltou;
-
-let unknownValue: unknown;
-unknownValue = 3;
-unknownValue = 'opa';
-unknownValue = true;
-unknownValue = 'vai sim'
-
-let stringTest2: string = 'agora vai'
-//stringTest2 = unknownValue;
-
-if(typeof unknownValue === 'string'){
-    stringTest2 = unknownValue;
-}
-
-function jogaErro(erro: string, codigo: number): never {
-    throw {error: erro, code: codigo};
-}
-
-jogaErro('deu erro', 500);
+buttonTest?.addEventListener('click', () => {
+    console.log('Funcionou');
+});
